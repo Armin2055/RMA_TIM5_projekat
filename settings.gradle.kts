3a65954f07e826c34c9f64c54dec6e6f9bd67152
CetvrtiDvaA <CetvrtiDvaA@gimnazija.local>
2024-03-22 10:16:54 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "RMA_TIM5_projekat"
+include(":app")
+ 
